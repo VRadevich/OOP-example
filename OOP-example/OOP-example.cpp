2b9afedffff802f8b5296e2a09427006aaf26ada
morozov603v1@gmail.com <morozov603v1@gmail.com>
2020-06-30 21:21:40 +0700

    Second inherited class added

--- OOP-example/OOP-example.cpp
+++ OOP-example/OOP-example.cpp
@@ -11,11 +11,12 @@ namespace nsEx1
     {
     // Секция определения членов класса.
     public:  
+        Ex1()=default;
         Ex1(int A, int B);
-        void Swap_ab( void );
-        int get_a( void );
-        int get_b( void );
-    private:
+        void Swap_ab(void);
+        int get_a(void);
+        int get_b(void);
+    protected:
         int a,b;
     };
     // Конец секции.
@@ -26,7 +27,7 @@ namespace nsEx1
         b = B;
     }
 
-    void Ex1::Swap_ab( void ) {
+    void Ex1::Swap_ab(void) {
         int swapHelper = a; // Свап значений через третью переменную.
         a = b;
         b = swapHelper;     
@@ -40,16 +41,107 @@ namespace nsEx1
         return b;
     }
     // Конец секции.
+
+    // Унаследованный класс.
+    class Ex2: public Ex1 {
+    // Секция определения членов класса.
+    public:
+        Ex2(int A, int B, int C); 
+        int get_c(void);
+        void Swap_ac(void);
+        void Swap_bc(void);
+    private:
+        int  c;
+    };
+    // Конец секции.
+
+    // Секция определения функций и конструкторов.
+    Ex2::Ex2(int A, int B, int C) {
+        a = A;
+        b = B;
+        c = C;
+    }
+
+    int Ex2::get_c(void) {
+        return c;
+    }
+
+    void Ex2::Swap_ac(void) {
+        int swapHelper = a; // Свап значений через третью переменную.
+        a = c;
+        c = swapHelper;
+    }
+
+    void Ex2::Swap_bc(void) {
+        int swapHelper = b; // Свап значений через третью переменную.
+        b= c;
+        c = swapHelper;
+    }
+    // Конец секции.
 }
 using namespace nsEx1;
 
 int main()
 {
-    Ex1 Example1(10,15);
-    Ex1 Example2(20, 25);
-    cout << "A in first Example: " << Example1.get_a() << " B in second Example: " << Example2.get_b() << endl;
-    Example1.Swap_ab();
-    Example2.Swap_ab();
-    cout << "A in first Example: " << Example1.get_a() << " B in second Example: " << Example2.get_b() << endl;
+    // Объявление экземпляров классов
+    Ex1 Example1_1(10,15);
+    Ex1 Example1_2(20, 25);
+    Ex2 Example2_1(3, 6, 9);
+    Ex2 Example2_2(7, 14, 21);
+
+    cout<< "Instances of class examples was declared succsessfully" <<endl;
+
+    cout << "A in first Example1: " << Example1_1.get_a() << endl
+        << "B in first Example1: " << Example1_1.get_b() << endl;
+
+    cout << "A in second Example1: " << Example1_2.get_a() << endl
+        << "B in second Example1: " << Example1_2.get_b() << endl;
+
+    cout << "A in first Example2: " << Example2_1.get_a() << endl
+        << "B in first Example2: " << Example2_1.get_b() << endl
+        << "C in first Example2: " << Example2_1.get_c() << endl;
+
+    cout << "A in second Example2: " << Example2_2.get_a() << endl
+        << "B in second Example2: " << Example2_2.get_b() << endl
+        << "C in second Example2: " << Example2_2.get_c() << endl;
+
+    // Свапы
+
+    Example1_1.Swap_ab();
+    Example1_2.Swap_ab();
+    cout<< "As and Bs swaped in all class Example1 instances" << endl;
+
+    cout << "A in first Example1: " << Example1_1.get_a() << endl
+        << "B in first Example1: " << Example1_1.get_b() << endl;
+
+    cout << "A in second Example1: " << Example1_2.get_a() << endl
+        << "B in second Example1: " << Example1_2.get_b() << endl;
+
+    Example2_1.Swap_ac();
+    Example2_2.Swap_ac();
+    cout << "As and Cs swaped in all class Example2 instances" << endl;
+
+    cout << "A in first Example2: " << Example2_1.get_a() << endl
+        << "B in first Example2: " << Example2_1.get_b() << endl
+        << "C in first Example2: " << Example2_1.get_c() << endl;
+
+    cout << "A in second Example2: " << Example2_2.get_a() << endl
+        << "B in second Example2: " << Example2_2.get_b() << endl
+        << "C in second Example2: " << Example2_2.get_c() << endl;
+
+    Example2_1.Swap_bc();
+    Example2_2.Swap_bc();
+    cout << "Bs and Cs swaped in all class Example2 instances" << endl;
+
+    cout << "A in first Example2: " << Example2_1.get_a() << endl
+        << "B in first Example2: " << Example2_1.get_b() << endl
+        << "C in first Example2: " << Example2_1.get_c() << endl;
+
+    cout << "A in second Example2: " << Example2_2.get_a() << endl
+        << "B in second Example2: " << Example2_2.get_b() << endl
+        << "C in second Example2: " << Example2_2.get_c() << endl;
+
+
+
 }
 
